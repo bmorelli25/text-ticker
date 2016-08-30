41c2323e25dcfb895ceb76df60fc8dbac492215d
pentel13 <bmorelli25@gmail.com>
2016-08-29 19:43:03 -0700

    initial commit

---
+++ src/TextInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+var TextInput = React.createClass({
+  render: function () {
+    return(
+      <div>
+        Text input here
+      </div>
+    );
+  }
+});
+
+module.exports = TextInput;
---
+++ src/TextOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+var TextOutput = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Text output here
+      </div>
+    );
+  }
+});
+
+module.exports = TextOutput;
